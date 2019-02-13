bb59c9d2f927a3c43585e0c3b1a2a89c2b4f0674
Diablx <kajetanboruta@gmail.com>
2019-02-13 19:42:43 +0100

    Add files via upload

---
+++ ReadRss/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Documents;
+using System.Diagnostics;
+
+namespace ReadRss
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Link_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                string path;
+                path = (sender as Hyperlink).Tag as string;
+                try
+                {
+                    Process.Start(path);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(path);
+                }
+            }
+        }
+    }
+}
